27d886914af46336a839a83d76f62d1de1fd585b
ahmedElnewery <ahmedm.elnewery@gmail.com>
2021-02-22 00:28:36 +0200

    No.2

--- day1/app.js
+++ day1/app.js
@@ -30,3 +30,24 @@ console.log(compare2Objs({ age: 25, hair: 'long', beard: true }, { hair: 'long',
 console.log(compare2Objs({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
 console.log(compare2Objs({ hair: 'long', beard: true }, { age: 26, hair: 'long', beard: true })); // false
 /******************************************************************************************/
+// No2 
+// Write a JavaScript program to copy a string to the clipboard.
+
+var copyTextareaBtn = document.querySelector('#copyBtn');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  const virtualTextArea =  document.createElement("textarea")
+  const text = document.getElementById("textCodied").textContent
+  virtualTextArea.textContent = text
+  document.body.append(virtualTextArea);
+  virtualTextArea.style.position = "fixed"
+  virtualTextArea.style.left = "-10000000px"
+  virtualTextArea.select();
+  virtualTextArea.focus();
+    const successful = document.execCommand('copy');
+    if(successful){
+    console.log("get copied")
+    document.body.removeChild(virtualTextArea);
+    }
+    
+});
