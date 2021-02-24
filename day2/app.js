1c308eb820ef8371e8fd601048b36888cfa4365b
ahmedElnewery <ahmedm.elnewery@gmail.com>
2021-02-24 16:46:35 +0200

    No.6

--- day2/app.js
+++ day2/app.js
@@ -1,14 +1,31 @@
-function findKey(obj,key){
 
-console.log("giug")
+//No.6 Write a JavaScript program to target a given value in a nested JSON object, based on the given key.
 
+function findKey(obj, key) {
+  let value;
+  if (key in obj) {
+    return obj[key];
+  } else {
+    Object.values(obj).forEach((digObj) => {
+      typeof digObj === "object"
+        ? (value = findKey(digObj, key))
+        : (value = undefined);
+    });
+  }
+  return value;
 }
-
+//testing
 const data = {
   level1: {
     level2: {
-      level3: 'some data'
-    }
-  }
+      level3: "some data",
+    },
+  },
 };
-console.log(findKey(data,key))
+const dog = {
+  "status": "success",
+  "message": "https://images.dog.ceo/breeds/african/n02116738_1105.jpg"
+}
+console.log(findKey(data, "level3"));
+console.log(findKey(dog, 'message'));
+/***********************************************************************/
